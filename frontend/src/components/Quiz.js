9f10ecca84985fad53ba18ca705a97ea19a3c563
Adeola Quadri <adeolaakorede27@gmail.com>
2025-04-13 19:04:12 +0100

    Modified Quiz.js in the frontend

--- frontend/src/components/Quiz.js
+++ frontend/src/components/Quiz.js
@@ -1,4 +1,125 @@
-<div className="min-h-screen bg-gray-100 flex items-center justify-center px-4 sm:px-6 py-6">
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer"
+
+const Quiz = () => {
+    const { quizId } = useParams();
+    const [questions, setQuestions] = useState([]);
+    const [image, setImage] = useState("")
+    const [quizInfo, setQuizInfo] = useState({});
+    const [selectedOptions, setSelectedOptions] = useState({});
+    const [currentPage, setCurrentPage] = useState(1);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const token = localStorage.getItem("token");
+                const res = await axios.get(`https://quiz-app-0yfq.onrender.com/question/${quizId}`, 
+                     {headers: {
+                    Authorization: `Bearer ${token}`,
+                  },});
+                if (res.data.user) {
+                setIsLoggedIn(true);
+                setQuestions(res.data.questions);
+                setQuizInfo(res.data.quiz); // assuming quiz info has logo and techName
+                setUser(res.data.user);
+                }else{
+                    window.location.href = "/login"
+                }
+            } catch (err) {
+                console.error(err);
+                navigate("/login");
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [quizId]);
+    
+    useEffect(() => {
+        const fetchImage = async () => {
+            try {
+                const token = localStorage.getItem("token");
+                const res = await axios.get(`http://localhost:4030/getImage/${quizId}`,  {headers: {
+                    Authorization: `Bearer ${token}`,
+                  },});
+                setImage(res.data.image);
+            } catch (err) {
+                setIsLoggedIn(false);
+                console.error(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchImage();
+    }, [quizId]);
+
+    
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        navigate("/login");
+      };
+
+    const handleOptionChange = (questionId, option) => {
+        setSelectedOptions((prev) => ({
+            ...prev,
+            [questionId]: option,
+        }));
+    };
+
+    const nextPage = () => {
+        if (!selectedOptions[questions[currentPage - 1]._id]) {
+            alert("Please select an option before proceeding!");
+            return;
+        }
+        setCurrentPage((prev) => prev + 1);
+    };
+
+    const prevPage = () => {
+        setCurrentPage((prev) => prev - 1);
+    };
+
+    const handleSubmit = async () => {
+        let score = 0;
+        questions.forEach((q) => {
+            if (selectedOptions[q._id] === q.correctAnswer) {
+                score += 1;
+            }
+        });
+
+        const totalQuestions = questions.length;
+        const percentage = (score / totalQuestions) * 100;
+        const username = user?.username;
+        alert(`Dear ${username}, your score is ${score}/${totalQuestions}`);
+
+        try {
+            await axios.post(
+                "http://localhost:4030/add-score",
+                { username, quizId, score, totalQuestions, percentage },
+                { withCredentials: true }
+            );
+        } catch (err) {
+            console.error("Failed to submit score:", err);
+        }
+    };
+
+    if (loading) return <p className="text-center mt-10 text-xl">Loading...</p>;
+
+    const currentQuestion = questions[currentPage - 1];
+
+    return (
+        <>  
+       <Header username={user.username} email={user.email} isLoggedIn={isLoggedIn} onLogout={handleLogout}/>     
+       <div className="min-h-screen bg-gray-100 flex items-center justify-center px-4 sm:px-6 py-6">
   <div className="bg-[#0f172a] text-white rounded-xl shadow-xl w-full max-w-4xl flex flex-col overflow-hidden break-words">
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
       
@@ -71,3 +192,10 @@
     </div>
   </div>
 </div>
+
+        <Footer />
+        </>
+    );
+};
+
+export default Quiz;
